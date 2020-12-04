a0e2376dcd09ee1ad948d25fb518580e95d4330e
Thomas Kim <Thomasjk0831@gmail.com>
2020-12-04 17:39:41 -0500

    installed packaged

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+// Update with your config settings.
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/items.db3",
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/items.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+};
