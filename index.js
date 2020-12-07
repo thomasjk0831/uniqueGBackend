75ca5a337f13375d62cb30596c0ac1f0ad72a5e5
Thomas Kim <Thomasjk0831@gmail.com>
2020-12-07 11:08:55 -0500

    fd

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const server = require("./api/server.js");
-const wakeUpDyno = require('wokeDyno.js')
+const wakeUpDyno = require('./wokeDyno.js')
 const port = process.env.PORT || 5000
 
-const DYNO_URL = "https://uniquegifts-3budisus1.vercel.app/"
+const DYNO_URL = "https://backend-uniquegifts.herokuapp.com/"
 
 server.listen(port, () => {
-    // wakeUpDyno(DYNO_URL)
+    wakeUpDyno(DYNO_URL)
     console.log("api up")
 })
