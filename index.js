9690a587b979f26d41080385d1fd46ae140d746f
Thomas Kim <Thomasjk0831@gmail.com>
2020-12-07 10:49:22 -0500

    add code to prevent heorku sleep

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const server = require("./api/server.js");
-
+const wakeUpDyno = require('wokeDyno.js')
 const port = process.env.PORT || 5000
 
+const DYNO_URL = "https://uniquegifts-3budisus1.vercel.app/"
+
 server.listen(port, () => {
-    console.log(`Running on port: ${port}`)
+    wakeUpDyno(DYNO_URL)
 })
