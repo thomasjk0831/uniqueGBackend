4407c08215ebf4132a86670e8fc213d1a026a1f0
thomasjk0831 <thomaskim08310@gmail.com>
2021-04-22 15:22:49 -0400

    dyno

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const server = require("./api/server.js");
-const wakeUpDyno = require('./wokeDyno.js')
-const port = process.env.PORT || 5000
+const wakeUpDyno = require("./wokeDyno.js");
+const port = process.env.PORT || 5000;
 
-const DYNO_URL = "https://backend-uniquegifts.herokuapp.com/"
+const DYNO_URL = "https://backend-uniquegifts.herokuapp.com/";
 
 server.listen(port, () => {
-    wakeUpDyno(DYNO_URL)
-    console.log("api up")
-})
+  // wakeUpDyno(DYNO_URL)
+  console.log("api up");
+});
